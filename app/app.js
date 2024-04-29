de373163b09f3dd287ffe44fc659c19f409061c1
Conrado1212 <conrado1212@gmail.com>
2024-04-29 18:34:59 +0200

    menu overview

--- app/app.js
+++ app/app.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
-    var NavY = $('.top-menu').offset().top;
+// $(document).ready(function() {
+//     var NavY = $('.top-menu').offset().top;
 
-    var stickyNav = function(){
-        var ScrollY = $(window).scrollTop();
-        ScrollY > NavY ?  $('.top-menu').addClass('sticky') : $('.top-menu').removeClass('sticky');
-    };
+//     var stickyNav = function(){
+//         var ScrollY = $(window).scrollTop();
+//         ScrollY > NavY ?  $('.top-menu').addClass('sticky') : $('.top-menu').removeClass('sticky');
+//     };
 
-    stickyNav();
+//     stickyNav();
 
-    $(window).scroll(function() {
-        stickyNav();
-    });
-});
+//     $(window).scroll(function() {
+//         stickyNav();
+//     });
+// });
 
 
 function scrollToElement(x,y){
@@ -27,3 +27,51 @@ scrollToBtn.click(() => {
 });
 }
 onclick="document.getElementById('about').scrollIntoView({ behavior: 'smooth'})"
+
+function getid(obj) {
+  alert(obj.id);
+}
+
+//  onmouseover="getid(this);" 
+
+
+window.addEventListener('load',function(){
+  $('#menu li').click(function () {
+      $('#menu li').removeClass('active');
+
+    $(this).addClass('active');
+  })
+})
+
+// get the actual pathname:
+var path = location.pathname;
+// filter menu items to find one that has anchor tag with matching href:
+$('#menu li').filter(function(){
+    return '/' + $('a', this).attr('href') === path;
+// add class active to the item:
+}).addClass('active');
+
+function menuU(){
+    var toggleMenuU = document.querySelector('.menuU');
+    //console.log(toggleMenuU);
+    toggleMenuU.classList.toggle('active2');
+}
+
+
+document.addEventListener("click", function(e){
+    
+  var MenuU = document.querySelector('.menuU.active2');
+  var toggleMenuU2 = document.querySelector('.fa-regular.fa-user');
+  console.log(e.target);
+  if(typeof(MenuU) !='undefined' && MenuU !=null){
+  if(e.target !==MenuU && e.target !==toggleMenuU2){
+      MenuU.classList.remove('active2');
+     // toggleMenuU.style.visibility = "hidden";
+      console.log(e.target);
+      //&& e.target  !==toggleMenuU2
+     // console.log(toggleMenuU.classList.contains('active2'));
+     //&& toggleMenuU.classList.contains('active2') 
+     //check tomorrow if contains will be works for this shit
+  }
+  }
+});
